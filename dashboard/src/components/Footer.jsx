c29a3239a651ffbeeaceaf6071462376aeb9a6be
Divine Higgins <dhiggins94@gmail.com>
2022-06-21 20:10:07 -0400

    added colormapping and financial

--- dashboard/src/components/Footer.jsx
+++ dashboard/src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 E-shopper.com
+      </p>
+    </div>
   )
 }
--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ export default function Sidebar() {
               onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span> Shoppy</span>
+              <SiShopware /> <span> E-shopper</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
